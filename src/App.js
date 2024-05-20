736c1882b822171ff532e61070db36cfc84acf0e
Jeffrey Zhou <zhouziyu925@gmail.com>
2024-05-20 00:35:31 -0600

    add and test a Dark mode button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState, React } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //Components
 import TopNavigationBar from './components/NavigationBar';
@@ -7,34 +8,57 @@ import HomePage from './pages/Home/HomePage';
 import LandingPage from './pages/Home/LandingPage';
 
 //antD
-import { Layout } from 'antd';
+import { Layout, ConfigProvider, theme, Button } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 
 
+
 function App() {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+   };
+
   return (
-    <BrowserRouter>
-      <Layout>
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+      }}>
+        
+        <BrowserRouter>
+          <Layout>
 
-        <Header>
-          <TopNavigationBar />
-        </Header>
+            <Header>
+              <TopNavigationBar />
+              
+            </Header>
 
-        <Layout>
-          {/* <Sider>left sidebar</Sider> */}
-          <Content>
-            <Routes>
-              <Route path='/home' element={<HomePage />}/>
-              <Route path='/landing' element={<LandingPage />}/>
-            </Routes>
-          </Content>
-        </Layout>
+            <Layout>
+              {/* <Sider>left sidebar</Sider> */}
+              <Content>
+                <Routes>
+                  <Route path='/home' element={<HomePage />}/>
+                  <Route path='/landing' element={<LandingPage />}/>
+                </Routes>
+              </Content>
+            </Layout>
 
-        <Footer>footer</Footer>
+            <Footer>
+              <Button onClick={handleClick}>
+                Change Theme to {isDarkMode ? "Light" : "Dark"}
+              </Button>
+            </Footer>
 
-      </Layout>
-    </BrowserRouter>
+          </Layout>
+        </BrowserRouter>
+
+
+
+   </ConfigProvider>
+    
     
   );
 }
